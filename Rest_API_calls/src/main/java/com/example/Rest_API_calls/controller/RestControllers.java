b2131a29e20e03f3dbb2abda8515220b872f841e
vinayjadaun11112003 <vinayjadaun11112003@gmail.com>
2025-02-22 10:43:32 +0530

    [Vinay Jadaun] - Add. UC04 POST Request from Request Body

--- Rest_API_calls/src/main/java/com/example/Rest_API_calls/controller/RestControllers.java
+++ Rest_API_calls/src/main/java/com/example/Rest_API_calls/controller/RestControllers.java
@@ -23,6 +23,26 @@ public class RestControllers {
     public String sayHellooo(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
     }
+
+    //UC04 : GET Request by RequestBody from /post and convert it to the java object by using internal deserialization.
+    @PostMapping("/post")
+    public String postmethod(@RequestBody NameRequest request) {
+        return "Hello " + request.getName() + " from BridgeLabz";
+    }
+}
+
+//Name Request which will Convert RequestBody raw json data to java object.
+class NameRequest {
+    private String name;
+
+    // Getter and Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
 
 
